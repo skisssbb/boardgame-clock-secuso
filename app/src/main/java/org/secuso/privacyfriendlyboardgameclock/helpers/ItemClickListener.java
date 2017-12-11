04ddbf45acb5a1964b0f78a642bb7f1529494016
Bi Banh Bao <skisssbb@hotmail.de>
2017-12-11 02:29:57 +0100

    Select Mode + Remove
    - FAB done

--- app/src/main/java/org/secuso/privacyfriendlyboardgameclock/helpers/ItemClickListener.java
+++ app/src/main/java/org/secuso/privacyfriendlyboardgameclock/helpers/ItemClickListener.java
@@ -9,5 +9,6 @@ import android.view.View;
  */
 
 public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
+    void onItemClick(View view, int position);
+    boolean onItemLongClicked(View view, int position);
 }
--- app/src/main/java/org/secuso/privacyfriendlyboardgameclock/helpers/ScrollAwareFABBehavior.java
+++ app/src/main/java/org/secuso/privacyfriendlyboardgameclock/helpers/ScrollAwareFABBehavior.java
@@ -2,6 +2,7 @@ package org.secuso.privacyfriendlyboardgameclock.helpers;
 
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
@@ -9,6 +10,8 @@ import android.util.AttributeSet;
 import android.view.View;
 
 /**
+ * Describe the behavior of FAB when scrolled
+ * FAB should disappear while scrolling down and reappear while scrolling up
  * Created by Quang Anh Dang 03.12.2017.
  * https://guides.codepath.com/android/floating-action-buttons#converting-from-listview-to-recyclerview
  */
@@ -19,23 +22,41 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
         super();
     }
 
+    /**
+     * the default behavior is used for the Floating Action Button to make room for the Snackbar
+     * We want to extend this behavior to signal that we wish to handle scroll events in the vertical direction
+     * @param coordinatorLayout
+     * @param child
+     * @param directTargetChild
+     * @param target
+     * @param axes
+     * @param type
+     * @return
+     */
     @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
-                                       FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
-                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
-                        nestedScrollAxes);
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child,
+                                       @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
     }
 
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
-                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
-                dyUnconsumed);
-
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target,
+                               int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+        // User scrolled down -> hide the FAB
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
-            child.hide();
-        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            // Workaround, cause child.hide also set View to GONE -> later not considered anymore
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+
+        }
+        // User scrolled up -> show the FAB
+        else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             child.show();
         }
     }
